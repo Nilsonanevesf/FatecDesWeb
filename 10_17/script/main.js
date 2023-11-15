c1d1acb6d64da0b05bc9b07e753c714562151bf6
Nilsonanevesf <nilson.anf@gmail.com>
2023-11-14 21:48:33 -0300

    update 11_14

--- 10_17/script/main.js
+++ 10_17/script/main.js
@@ -1,13 +1,46 @@
 var $nome = document.getElementById('nome');
 let quantidadePacotes = document.querySelectorAll("input[name='pacotes']").length;
 var $pacotesArray = document.querySelectorAll("input[name='pacotes']");
+var $adicionais = document.getElementsByClassName('adicionais');
+var $alimentacaoCheck = document.getElementById('c1');
+var $veiculoCheck = document.getElementById('c2');
+var $transladoCheck = document.getElementById('c3');
+var $output= document.getElementById('output');
 function calcular(){
     
     console.log(quantidadePacotes)
     
     console.log($pacotesArray)
 
-    for(var i=0;i<quantidadePacotes;i++){
-        console.log($pacotesArray[i].value);
+    let pacoteValue = document.querySelectorAll("input[name='pacotes']:checked")[0]? Number(document.querySelectorAll("input[name='pacotes']:checked")[0].value) : 0;
+
+
+    pacoteValue = calcularAdicionais(pacoteValue);
+    
+
+    $output.textContent = `Caro(a) ${$nome.value}. Seu valor final é R$${pacoteValue}` 
+    // for(var i=0;i<quantidadePacotes;i++){
+    //     if($pacotesArray[i].checked){
+    //         console.log($pacotesArray[i].value);
+    //     }
+    // }
+}
+
+function calcularAdicionais(valorInicial){
+
+    let valorFinal = valorInicial
+
+    if($alimentacaoCheck.checked){
+        valorFinal = valorFinal + Number($alimentacaoCheck.value)
     }
+
+    if($veiculoCheck.checked){
+        valorFinal = valorFinal + Number($veiculoCheck.value)
+    }
+
+    if($transladoCheck.checked){
+        valorFinal = valorFinal + Number($transladoCheck.value)
+    }
+
+    return valorFinal
 }
